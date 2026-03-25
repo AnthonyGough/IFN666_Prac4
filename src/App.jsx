1ef753fb02acda2e502f3cfa992c35a0f1b6e882
Ubuntu <ubuntu@ip-172-31-106-144.ap-southeast-2.compute.internal>
2026-03-25 11:54:53 +0000

    Prac4_Simple_Components

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+
+import './App.css'
+
+const animals = [
+ {
+   name: "Lion",
+   number: 3,
+   eats: ["zebra", "antelope", "buffalo", "hippopotamus"]
+ },
+ {
+   name: "Tiger",
+   number: 5,
+   eats: ["moose", "deer", "buffalo"]
+ },
+ {
+   name: "Giraffe",
+   number: 6,
+   eats: ["leaves", "twigs"]
+ },
+ {
+   name: "Elephant",
+   number: 4,
+   eats: ["grass", "leaves", "flowers", "fruit"]
+ },
+ {
+   name: "Monkey",
+   number: 10,
+   eats: ["fruit", "leaves", "vegetables", "insects"]
+ },
+ {
+   name: "Lemur",
+   number: 15,
+   eats: ["fruit", "leaves", "insects"]
+ },
+ {
+   name: "Rhinoceros",
+   number: 2,
+   eats: ["grass", "shoots", "leaves", "berries"]
+ }
+];
+
+function AnimalComponent(props) {
+    return (
+    <div key={props.number}>
+    <h1> Name: {props.name}</h1>
+    <p>Number: {props.number}</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+<>
+    {animals.map(animal => (
+      <AnimalComponent {...animal} key={animal.number}/>
+    ))}
+
+</>
+  )
+
+   
+
+}
+
+export default App
